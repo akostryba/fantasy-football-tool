659d5eecba684e4275c7d7eed1f11742fd6fcd91
Andrew Kostryba <ankostryb@gmail.com>
2023-11-09 20:27:30 -0600

    Added scoring format and opponent

--- project/src/main.rs
+++ project/src/main.rs
@@ -5,61 +5,19 @@ use rocket_dyn_templates::{Template,context};
 use std::path::{Path,PathBuf};
 
 pub mod json;
+pub mod query;
+pub use query::{get_standard_projection, get_ppr_projection, get_halfppr_projection, request_fantasy, 
+                request_players, get_player,
+                request_schedule, get_game, get_opp_id,
+                request_teams, get_team};
+
 
 pub use json::my_structs;
-pub use json::my_structs::{PlayerBody, PlayerRoot};
 use serde::Serialize;
 use serde_json::Value;
-use lazy_static::lazy_static;
-
-//static strings
-lazy_static! {
-    static ref REQUEST_HOST : &'static str = "https://tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com";
-    static ref BETTING : &'static str = "/getNFLBettingOdds?";
-    static ref SCHEDULE : &'static str = "/getNFLGamesForWeek?";
-    static ref FANTASY : &'static str = "/getNFLProjections?";
-    static ref PLAYERS : &'static str = "/getNFLPlayerList";
-    static ref API_KEY : &'static str = "7edefd3921mshf363ed62e8f497bp1ddf5bjsnd14740483cb3";
-}
-
-fn get_player<'a>(player_vec : &'a Vec<PlayerBody>, target_name : &'a str) -> Option<&'a PlayerBody> {
-  return player_vec.iter().find(|player| player.long_name == target_name);
-}
-
-fn get_player_id (player : &PlayerBody) -> &String {
-  return &player.player_id;
-}
-
-fn get_ppr_projection<'a> (player_id: &'a String, json : &'a Value) -> &'a Value {
-  return &json["body"]["playerProjections"][player_id]["fantasyPointsDefault"]["PPR"];
-}
 
 fn create_client () -> Client{
   return reqwest::Client::new();
-}
-
-async fn request_players (client : &Client) -> Result<(PlayerRoot), Box<dyn std::error::Error>>{
-  let players_request : String = format!("{}{}", *REQUEST_HOST, *PLAYERS);
-  let players_response = client.get(players_request)
-        .header("X-RapidAPI-Key",
-        *API_KEY)
-        .send()
-        .await?;
-  let players = players_response.json::<my_structs::PlayerRoot>().await?;
-  Ok(players)
-}
- 
-async fn request_fantasy (client: &Client, week: &str) -> Result<(Value), Box<dyn std::error::Error>> {
-  let fantasy_param = vec![("week", week)];
-  let fantasy_request : String = format!("{}{}", *REQUEST_HOST, *FANTASY);
-  let fantasy_response = client.get(fantasy_request)
-        .header("X-RapidAPI-Key",
-        *API_KEY)
-        .query(&fantasy_param)
-        .send()
-        .await?;
-  let data = fantasy_response.json::<serde_json::Value>().await?;
-  Ok(data)
 }
 
 #[macro_use] extern crate rocket;
@@ -67,7 +25,10 @@ async fn request_fantasy (client: &Client, week: &str) -> Result<(Value), Box<dy
 
 #[get ("/")]
 fn index () -> Template {
-  Template::render ("test2", context! { })
+  Template::render ("test2", context! { 
+    default_player1 : String::from("Stefon Diggs"),
+    default_player2 : String::from("Michael Thomas"),
+  })
 }
 
 #[derive (Debug,FromForm)]
@@ -75,6 +36,7 @@ struct Input<'r> {
   player1 : &'r str, 
   player2 : &'r str,
   week : &'r str,
+  scoring : &'r str,
 }
 
 #[post ("/analyze", data = "<s>")]
@@ -89,17 +51,26 @@ async fn analyze (s : Form<Input<'_>>) -> Template {
   };
   let player1 = match get_player(&players.body, s.player1){
     Some(p) => p,
-        None => {
-            return Template::render ("test2", context! { });
-        }
+    None => {
+      return Template::render ("test2", context! { 
+        default_player1 : String::from("Player not found"),
+        default_player2 : String::from("Michael Thomas"),
+      });
+      }
     };
+  println!("Player 1 Found: {}", player1.player_id);
 
-    let player2 = match get_player(&players.body, s.player2){
-      Some(p) => p,
-          None => {
-              return Template::render ("test2", context! { });
-          }
-      };
+  let player2 = match get_player(&players.body, s.player2){
+    Some(p) => p,
+    None => {
+      return Template::render ("test2", context! { 
+        default_player1 : String::from("Stefon Diggs"),
+        default_player2 : String::from("Player not found"),
+      });
+      }
+    };
+  
+    println!("Player 2 Found: {}", player2.player_id);
 
   let fantasy_data = match request_fantasy(&client, s.week).await{
     Ok(d) => d,
@@ -107,15 +78,71 @@ async fn analyze (s : Form<Input<'_>>) -> Template {
       return Template::render ("test2", context! { });
     }
   };
-  let ppr_proj1 = get_ppr_projection(&player1.player_id, &fantasy_data);
-  let ppr_proj2 = get_ppr_projection(&player2.player_id, &fantasy_data);
+
+  let proj1 = match s.scoring{
+    "Standard" => get_standard_projection(&player1.player_id, &fantasy_data),
+    "PPR" => get_ppr_projection(&player1.player_id, &fantasy_data),
+    "Half PPR" => get_halfppr_projection(&player1.player_id, &fantasy_data),
+    _ => &Value::Null
+  };
+
+  let proj2 = match s.scoring{
+    "Standard" => get_standard_projection(&player2.player_id, &fantasy_data),
+    "PPR" => get_ppr_projection(&player2.player_id, &fantasy_data),
+    "Half PPR" => get_halfppr_projection(&player2.player_id, &fantasy_data),
+    _ => &Value::Null
+  };
+
+  let schedule1 = match request_schedule(&client, &player1.team_id).await {
+    Ok(s) => s,
+    _ => return Template::render ("test2", context! { }),
+  };
+
+  let schedule2 = match request_schedule(&client, &player2.team_id).await {
+    Ok(s) => s,
+    _ => return Template::render ("test2", context! { }),
+  };
+
+  let game1 = match get_game(&s.week, &schedule1.body.schedule){
+    Some(g) => g,
+    None => return Template::render ("test2", context! { }),
+  };
+
+  let game2 = match get_game(&s.week, &schedule2.body.schedule){
+    Some(g) => g,
+    None => return Template::render ("test2", context! { }),
+  };
+
+  let opp1 = get_opp_id(&player1.team_id, game1);
+  let opp2 = get_opp_id(&player2.team_id, game2);
+
+  let teams = match request_teams(&client).await{
+    Ok(t) => t,
+    _ => return Template::render ("test2", context! { }),
+  };
+
+  let opp1_team = match get_team(&teams.body, opp1){
+    Some(t) => t,
+    None => return Template::render ("test2", context! { }),
+  };
+
+  let opp2_team = match get_team(&teams.body, opp2){
+    Some(t) => t,
+    None => return Template::render ("test2", context! { }),
+  };
+
+  println!("Score1 Found: {}, Score2 Found: {}", proj1, proj2);
   //format! ("Player 1 ID: {}. Projected PPR Points: {}\nPlayer 2 ID: {}. Projected PPR Points: {}", player1.player_id, ppr_proj1, player2.player_id, ppr_proj2)
   Template::render("test2", context! {
+    default_player1 : &player1.long_name,
+    default_player2 :&player2.long_name,
     player1_name: &player1.long_name,
     player2_name: &player2.long_name,
-    player1_proj : ppr_proj1,
-    player2_proj : ppr_proj2,
-    week : s.week
+    player1_proj : proj1,
+    player2_proj : proj2,
+    week : s.week,
+    opponent_1 : &opp1_team.team_name,
+    opponent_2 : &opp2_team.team_name,
   })
 }
 
